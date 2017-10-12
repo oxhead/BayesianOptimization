f479db3bf36e3305e8137ef9f76c9c100f6273da
Chin-Jung Hsu <chsu6@ncsu.edu>
2017-10-12 11:41:41 -0400

    Prototype to support sparse, discrete parameter space.

--- bayes_opt/__init__.py
+++ bayes_opt/__init__.py
@@ -1,4 +1,5 @@
 from .bayesian_optimization import BayesianOptimization
+from .discrete_bayesian_optimization import DiscreteBayesianOptimization
 from .helpers import UtilityFunction
 
-__all__ = ["BayesianOptimization", "UtilityFunction"]
+__all__ = ["BayesianOptimization", "DiscreteBayesianOptimization", "UtilityFunction"]
